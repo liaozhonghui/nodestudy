8fb2dbc78116042032d4ac4d2896ad359eeade25
zhonghui.liao <zhonghui.liao@zhongheschool.com>
2020-09-24 09:11:42 +0800

    add fp

--- FP/javascript.js
+++ FP/javascript.js
@@ -1,16 +1,79 @@
-var  _ = require('underscore');
-
-let res;
-res = _.max([1,2,3,4,5])
+var _ = require('underscore');
 
 function finder(valueFun, bestFun, coll) {
-    return _.reduce(coll, function(best, current) {
+    return _.reduce(coll, function (best, current) {
         var bestValue = valueFun(best);
         var currentValue = valueFun(current);
         return (bestValue === bestFun(bestValue, currentValue)) ? best : current;
     })
 }
 
-res = finder(_.identity, Math.max, [1, 2, 3, 4, 5]);
+let people = [{ name: 'fred', age: 65 }, { name: 'lucy', age: 36 }, { name: 'alpha', age: 40 }, { name: 'beta', age: 37 }];
+let arr = [2, 1, 4, 3, 5];
+{
+    let res = _.max([1, 2, 3, 4, 5]);
+    console.log('res', res);
+}
+{
+    let res = finder(_.identity, Math.max, arr);
+    console.log('res', res);
+}
+{
+    function plucker(FIELD) {
+        return function (obj) {
+            return (obj && obj[FIELD]);
+        }
+    }
+    let res = finder(plucker('age'), Math.max, people);
+    console.log('res', res);
+    {
+        function findL(x, y) {
+            return (x.charAt(0) === 'l' ? x : y);
+        }
+        let res = finder(plucker('name'), findL, people);
+        console.log('res', res);
+    }
+}
+{
+    function best(fun, coll) {
+        return _.reduce(coll, function (x, y) {
+            return fun(x, y) ? x : y;
+        })
+    }
+    {
+        let res = best(function (x, y) { return x > y; }, arr);
+        console.log('res', res);
+    }
+}
 
-console.log(res);
+{
+    // 重复，反复和条件迭代
+    function repeat(times, VALUE) {
+        return _.map(_.range(times), function () { return VALUE; })
+    }
+    {
+        let res = repeat(4, 'Major');
+        console.log('res', res);
+    }
+    function repeatly(times, fun) {
+        return _.map(_.range(times), fun);
+    }
+    {
+        let res = repeatly(3, function () { return Math.floor((Math.random() * 10) + 1) });
+        console.log('res', res);
+    }
+    function iterateUntil(fun, check, init) {
+        var ret = [];
+        var result = fun(init);
+
+        while (check(result)) {
+            ret.push(result);
+            result = fun(result);
+        }
+        return ret;
+    }
+    {
+        let res = iterateUntil(function (n) { return n + n; }, function (n) { return n <= 1024 }, 1);
+        console.log('res', res);
+    }
+}
--- FP/underscore.js
+++ FP/underscore.js
@@ -1,9 +1,7 @@
 /**
  * first
  */
-
-const { initial } = require("underscore");
-
+const _ = require('underscore');
 
 function first(array, n, guard) {
     if (array == null || array.length < 1) return n == null || guard ? void 0 : [];
@@ -13,4 +11,21 @@ function first(array, n, guard) {
 
 function initial(array, n, guard) {
     return slice.call(array, 0, Math.max(0, array.length - (n == null || guard ? 1 : n)));
+}
+
+{
+    let res = _.zip(['moe', 'larry', 'curly'], [30, 40, 50], [true, false, false]);
+    console.log(res);
+}
+{
+    let arr = _.map(_.range(10), function (v) { return v; })
+    console.log('arr', arr);
+    let chunkArr = _.chunk(arr, 2);
+    console.log('chunkArr', chunkArr);
+}
+
+{
+    var stooge = { name: 'moe' };
+    let res = stooge === _.constant(stooge)();
+    console.log('res', res);
 }
